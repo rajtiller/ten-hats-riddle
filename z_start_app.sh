2dc79d5319e33f3eb336219892dc905a56608618
rajtiller <raj.tiller@gmail.com>
2025-07-28 21:57:48 -0500

    initial commit

--- z_start_app.sh
+++ z_start_app.sh
@@ -4,11 +4,12 @@ repo_name="REMOTE-REPO-NAME"
 
 git add .
 git commit -m "initial commit"
-gh repo create $repo_name --public --source=. --remote=origin --push --web
+gh repo create $repo_name --public --source=. --remote=origin --push
 if [ $? -ne 0 ]; then
   gh auth login # Authenticate with GitHub CLI
-  gh repo create $repo_name --public --source=. --remote=origin --push --web
+  gh repo create $repo_name --public --source=. --remote=origin --push
 fi
+gh repo view --web  # opens the repo in browser
 git push -u origin master
 gh repo edit --default-branch master
 git push origin --delete main 
