707d46c9d2ac2695919f8f590e8203d1cad1e9c5
rajtiller <raj.tiller@gmail.com>
2025-07-28 21:50:05 -0500

    initial commit

--- z_start_app.sh
+++ z_start_app.sh
@@ -3,12 +3,11 @@ git init
 
 git add .
 git commit -m "initial commit"
-git checkout -b master
+gh auth login # Authenticate with GitHub CLI
+gh repo create REMOTE-REPO-NAME --public --source=. --remote=origin --push --web
 git push -u origin master
 gh repo edit --default-branch master
 git push origin --delete main 
-gh auth login # Authenticate with GitHub CLI
-gh repo create Ten-Hats-Riddle --public --source=. --remote=origin --push --web
 # if you forget to rename REMOTE-REPO-NAME, you can run: gh repo rename NEW-REPO-NAME
 
 npm install
